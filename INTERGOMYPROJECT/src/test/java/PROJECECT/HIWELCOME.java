a0d36c0c9727165d18f061c67dbf653febb441f6
Muneerarazak <shahnaz1612@gmail.com>
2022-08-18 17:19:13 +0530

    checking test

---
+++ INTERGOMYPROJECT/src/test/java/PROJECECT/HIWELCOME.java
@@ -0,0 +1,42 @@
+package PROJECECT;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HIWELCOME {
+	@Test
+	
+	public void hiwelcome() throws Throwable{
+		
+			 WebDriverManager.chromedriver().setup();
+			  WebDriver driver=new ChromeDriver();
+			  		  
+			
+		 FileInputStream fis=new FileInputStream("./data/common.properties"); 
+		Properties  p=new Properties();
+			 p.load(fis);
+			 System.out.println("LOGIN TO Application");
+			 String Url=p.getProperty("url");
+			 String username=p.getProperty("un");
+			 String password=p.getProperty("pwd");
+			 
+			driver.get(Url);
+			
+			
+			
+
+			
+		
+		
+		}
+	}
+
+
+
